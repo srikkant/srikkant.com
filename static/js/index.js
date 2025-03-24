b607693ea92e3680c99d772877ca56bfffbad786
Srikkant Srinivasan <srikkant@live.com>
2025-03-24 06:50:31 +0530

    chore: enabled ga

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBFJbVqGYB-CgDzfefT3ALFCDmHGRAjdDM",
+    authDomain: "srikkant.firebaseapp.com",
+    databaseURL: "https://srikkant.firebaseio.com",
+    projectId: "srikkant",
+    storageBucket: "srikkant.appspot.com",
+    messagingSenderId: "988675320014",
+    appId: "1:988675320014:web:27f383dc1e98ba081a089f",
+    measurementId: "G-JP3J1L17Z4",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 /**
  * @type NodeListOf<HTMLImageElement>
  */
